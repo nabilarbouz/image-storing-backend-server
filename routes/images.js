bc4d2fed19880ad0aecc754d7f198cedb8d4bdbe
Nabil Arbouz <43759350+nabilarbouz@users.noreply.github.com>
2021-11-21 18:48:14 -0800

    add CORS

--- routes/images.js
+++ routes/images.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 const {uploadFile, getFileStream} = require('../s3');
 
 const fs = require('fs')
 const util = require('util')
 const unlinkFile = util.promisify(fs.unlink)
 
-
 const multer = require("multer");
 const upload = multer({dest: "uploads/"});
 
-router.post('/', upload.single('image'), async (req, res ) => {
+var corsOptions = {
+    origin: 'http://project-3-frontend-lb-249883157.us-east-1.elb.amazonaws.com/'
+}
+
+router.post('/', cors(corsOptions), upload.single('image'), async (req, res ) => {
     const file = req.file;
     console.log(file)
     const result = await uploadFile(file);
